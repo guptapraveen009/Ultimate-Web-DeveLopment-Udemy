3c63dbf3686a99784ae1afb669d64071115cfba3
Gupta Praveen Kumar <149958810+guptapraveen009@users.noreply.github.com>
2025-12-07 11:11:10 +0530

    Update

--- Ultimate_Web_Dev/Javascript/Arrays.js
+++ Ultimate_Web_Dev/Javascript/Arrays.js
@@ -1,32 +1,32 @@
 // Arrays in js 
-const prompt = require("prompt-sync")()
-//Problems in Arrays 
-//Level - 1
-//Question -> Get the first item, the middle item and the last item of the array . 
+// const prompt = require("prompt-sync")()
+// //Problems in Arrays 
+// //Level - 1
+// //Question -> Get the first item, the middle item and the last item of the array . 
 
-let array = [1, 2, 3, 4, 5, 6];
-let first_elem = array[0];
-console.log(first_elem);
+// let array = [1, 2, 3, 4, 5, 6];
+// let first_elem = array[0];
+// console.log(first_elem);
 
-let middle = Math.floor(array.length / 2);
-let Middle = array[middle];
-console.log(Middle);
+// let middle = Math.floor(array.length / 2);
+// let Middle = array[middle];
+// console.log(Middle);
 
-let last_elem = array.length;
-console.log(last_elem);
+// let last_elem = array.length;
+// console.log(last_elem);
 
 
-//Question -> Declare an array called mixedDataTypes, put different data types in the array and find the length of the array. The array size should be greater than 5 
+// //Question -> Declare an array called mixedDataTypes, put different data types in the array and find the length of the array. The array size should be greater than 5 
 
-let mixedDataTypes = [1 , "Praveen" , admin = true ,  ,  obj = {
-        name : "Praveen",
-        Cource : "Full Stack Development",
-        Language : "JavaScript"
-}]
-for (let i = 0; i < mixedDataTypes.length; i++){
-console.log(typeof(mixedDataTypes)[i]);
-}
-console.log(mixedDataTypes.length);
+// let mixedDataTypes = [1 , "Praveen" , admin = true ,  ,  obj = {
+//         name : "Praveen",
+//         Cource : "Full Stack Development",
+//         Language : "JavaScript"
+// }]
+// for (let i = 0; i < mixedDataTypes.length; i++){
+// console.log(typeof(mixedDataTypes)[i]);
+// }
+// console.log(mixedDataTypes.length);
 
 // 1 -> Declare an array variable name itCompanies and assign initial values Facebook, Google, Microsoft, Apple, IBM, Oracle and Amazon
 // 2 -> Print the array using console.log()
@@ -37,7 +37,9 @@ console.log(mixedDataTypes.length);
 // 7 -> Print the array like as a sentence: Facebook, Google, Microsoft, Apple, IBM,Oracle and Amazon are big IT companies.
 // 8 -> Check if a certain company exists in the itCompanies array. If it exist return the company else return a company is not found
 // 9 -> Filter out companies which have more than one 'o' without the filter method
+// 10 -> Sort the array using sort() method
 
+//Answers
 // 1 -> Ans
 let itCompanies = ['FaceBook' , 'Google' , 'MicroSoft' , 'Apple' , 'IBM' , 'Oracle' , 'Amazon'];
 
@@ -77,14 +79,14 @@ console.log(sentence +" "+ "Are The Big IT Companies .");
 
 
 //8 -> Ans
-let Company_Name = prompt("Enter The Name Of The Company : ");
-let check = itCompanies.includes(Company_Name);
-if(check == true){
-        console.log(`${Company_Name} Is Present In The Array`);
-}
-else{
-        console.log("Company Not Found");
-}
+// let Company_Name = prompt("Enter The Name Of The Company : ");
+// let check = itCompanies.includes(Company_Name);
+// if(check == true){
+//         console.log(`${Company_Name} Is Present In The Array`);
+// }
+// else{
+//         console.log("Company Not Found");
+// }
 
 //9 -> Ans
 let result = []
@@ -95,3 +97,6 @@ for(let k = 0; k < itCompanies.length; k++){
         
 }
 console.log(result);
+
+//10 -> ans
+console.log(itCompanies.sort());
