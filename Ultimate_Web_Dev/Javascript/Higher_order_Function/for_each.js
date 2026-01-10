9930a64c51c101947536ed09e080a1d6c36f084f
Gupta Praveen Kumar <149958810+guptapraveen009@users.noreply.github.com>
2026-01-10 16:50:50 +0530

    Update

---
+++ Ultimate_Web_Dev/Javascript/Higher_order_Function/for_each.js
@@ -0,0 +1,13 @@
+//Question -> Use forEach to console.log each country in the countries array.
+// let countries  = ["India" , "Japan" , "Indoneasia" , "Riyad" , "USA" , "Germany" , "Italy"];
+// countries.forEach((country) => console.log(country));
+   
+
+//Question -> Use forEach to console.log each name in the names array.
+// let PersonName = ["Praveen" , "Rahul" , "Prem" , "Bharat" , "Aryan" , "Pratik"];
+// PersonName.forEach((Name) => console.log(Name));
+
+
+//Question -> Use forEach to console.log each number in the numbers array.
+// let number = [1 , 2  , 3 , 4 , 5 , 6 , 7 , 8];
+// number.forEach((Num) => console.log(Num));
