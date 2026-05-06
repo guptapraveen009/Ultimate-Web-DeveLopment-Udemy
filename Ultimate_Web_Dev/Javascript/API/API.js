856f0002a5b403f5a0eda5e5941c6df3699d8b01
Gupta Praveen Kumar <149958810+guptapraveen009@users.noreply.github.com>
2026-05-06 23:43:09 +0530

    Update

--- Ultimate_Web_Dev/Javascript/API/API.js
+++ Ultimate_Web_Dev/Javascript/API/API.js
@@ -22,3 +22,7 @@
 //       container.appendChild(p);
 //     })
 //   })
+
+
+
+
--- Ultimate_Web_Dev/Javascript/Class_Objects.js
+++ Ultimate_Web_Dev/Javascript/Class_Objects.js
@@ -1,4 +1,4 @@
-// const prompt = require("prompt-sync")()
+const prompt = require("prompt-sync")()
 
 // Objects
 // let obj = {
@@ -24,25 +24,41 @@
 // object.showMarks()
 
 // ProtoType and ProtoType Over Ridding
-let Intreset = {
-    info() {
-        let p = prompt("Enter The Principal Amount : ");
-        let r = prompt("Enter The Intreset Rate : ");
-        let t = prompt("Enter The time period : ");
-        let n = prompt("Enter The Intreset Preiod : ");
-        let A = p * Math.pow((1 + r / (n * 100)), n * t);
-        let CI = A - p;
-        console.log(`Total Amount: ${A.toFixed(2)}`);
-        console.log(`Interest: ${CI.toFixed(2)}`);
-    }
-}
+// let Intreset = {
+//     info() {
+//         let p = prompt("Enter The Principal Amount : ");
+//         let r = prompt("Enter The Intreset Rate : ");
+//         let t = prompt("Enter The time period : ");
+//         let n = prompt("Enter The Intreset Preiod : ");
+//         let A = p * Math.pow((1 + r / (n * 100)), n * t);
+//         let CI = A - p;
+//         console.log(`Total Amount: ${A.toFixed(2)}`);
+//         console.log(`Interest: ${CI.toFixed(2)}`);
+//     }
+// }
 
-let Depositer = {
-    Name: "shambhu Prasad Gupta",
+// let Depositer = {
+//     Name: "shambhu Prasad Gupta",
     
-}
-Object.setPrototypeOf(Depositer , Intreset)
-Depositer.info()
+// }
+// Object.setPrototypeOf(Depositer , Intreset)
+// Depositer.info()
+
+// if ("geolocation" in navigator) {
+//   // 2. Request the current position
+//   navigator.geolocation.getCurrentPosition(
+//     (position) => {
+//       const { latitude, longitude } = position.coords;
+//       console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
+//     },
+//     (error) => {
+//       console.error(`Error Code: ${error.code} - ${error.message}`);
+//     },
+//     { enableHighAccuracy: true, timeout: 5000 } // Optional configuration
+//   );
+// } else {
+//   console.log("Geolocation is not supported by this browser.");
+// }
 
 
-
+console.log(typeof NaN);
