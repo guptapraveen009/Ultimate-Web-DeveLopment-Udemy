863cb026fccfb3baf33dfdeced07f0827e69add6
Gupta Praveen Kumar <149958810+guptapraveen009@users.noreply.github.com>
2026-05-13 18:15:50 +0530

    Update

---
+++ Ultimate_Web_Dev/Javascript/Array/Main.js
@@ -0,0 +1,110 @@
+// Arrays in js 
+// const prompt = require("prompt-sync")()
+
+
+// // Question :  Get the first item, the middle item and the last item of the array
+// let arr = [1 , 2 , 3 , 4 , 5 , 6];
+// let first_value = arr[0];
+// console.log(first_value);
+// let Middle_value = Math.floor(arr.length / 2);
+// console.log(Middle_value);
+// let last = arr[arr.length - 1];
+// console.log(last);
+
+
+// // Question : 1 ->  Change each company name to uppercase one by one and print them out
+// //          : 2 -> Print the array like as a sentence: Facebook, Google, Microsoft, Apple, IBM,Oracle and Amazon are big IT companies.
+
+
+// let Companies = ["Apple" , "Microsoft" , "Deloitte" , "Cognizant" , "Infotech" , "Wipro"];
+
+// for(let company of Companies){   //for of is used In arrays
+//     let UpperCase = company.toUpperCase()
+//     console.log(UpperCase);
+// }
+
+
+// console.log(`${Companies.slice(1 , Companies.length - 1).join(',')} and ${Companies[Companies.length - 1]} are big IT Compaines`);
+
+
+
+// Question : Check if a certain company exists in the itCompanies array. If it exist return the company else return a company is not found
+
+//Note :  In This Question We Can Use Two Methods 1 -> Filter  , 2 -> Find , 
+// let itCompany = ["Apple" , "Microsoft" , "Deloitte" , "Cognizant" , "Infotech" , "Wipro" , "Oracle" , "Bussiness Next"];
+// let Find = itCompany.filter(Company => Company.includes("Apple")); //Filter Method
+// let Find = itCompany.find(company => company.includes("Apple"));
+// let CompanyName = prompt("Enter The Company Name : ");
+// if(itCompany.includes(CompanyName)) { 
+//     console.log(Find);
+// }else{
+//     console.log('Company Not Found');
+// }
+
+
+
+// Question : Filter out companies which have more than one 'o' without the filter method
+// Companies.forEach(words => {
+//   let count = words.toLowerCase().split('o').length - 1
+
+//   if(count > 1){
+//     console.log(words);
+//   }
+// })
+
+
+
+//Question ->  Slice out the first 3 companies from the array
+// let Slice = itCompany.slice(0 , Math.floor(itCompany.length / 2))
+// console.log(Slice);
+
+
+
+// Question : Slice out the last 3 companies from the array;
+// let SliceOut = itCompany.slice(Math.floor(itCompany.length / 2) , [itCompany.length])
+// console.log(SliceOut);
+
+
+// Question : Slice out the middle IT company or companies from the array
+// let Middle_Slice = itCompany[Math.floor(itCompany.length / 2)];
+// console.log(Middle_Slice);
+
+
+// Question : Remove out the middle IT company or companies from the array
+// let Mid = Math.floor(itCompany.length / 2);
+// let Middle_Pop = itCompany.splice(Mid , 1);
+// console.log(itCompany);
+
+
+// //Question : Remove the first IT company from the array
+// let first_pop = itCompany.shift()
+// console.log(itCompany);
+
+//Question :  Remove the last IT company from the array
+// let lastpop = itCompany.pop()
+// console.log(`${itCompany} and Last Company ${lastpop}`);
+
+
+//Question : Remove all IT companies
+// let all = itCompany.splice(0 , itCompany.length)
+// console.log(all);
+// console.log(itCompany);
+
+
+// Question : 
+// Create a separate countries.js file and store the countries array in to this file, create a separate file web_techs.js and store the webTechs array in to this file. Access both file in main.js file
+// import contries from"./countries.js"
+// import webTechs from"./web_tech.js"
+// console.log(webTechs);
+// console.log(contries);
+
+// Question : First remove all the punctuations and change the string to array and count the number of words in the array
+
+// let text = 'I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.'
+
+// console.log((/[.,/#!$%^&*;;{}=-_'~()]/g).test(text)); // -> it is used to check weather the punctutaion is there in sentence or not
+// let punctuation = text.replace(/[.,\/#!$%\^&\*;;{}=\-_'~()]/g , " ");
+// console.log(punctuation);
+// let word = punctuation.split(" ")
+// console.log(word);
+// console.log(word.length);
