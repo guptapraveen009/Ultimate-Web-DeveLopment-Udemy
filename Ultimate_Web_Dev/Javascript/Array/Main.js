b28d144bdfc1e255858967b8720ba9121f1191fb
Gupta Praveen Kumar <149958810+guptapraveen009@users.noreply.github.com>
2026-05-27 12:45:44 +0530

    Update

--- Ultimate_Web_Dev/Javascript/Array/Main.js
+++ Ultimate_Web_Dev/Javascript/Array/Main.js
@@ -1,5 +1,5 @@
 // Arrays in js 
-// const prompt = require("prompt-sync")()
+const prompt = require("prompt-sync")()
 
 
 // // Question :  Get the first item, the middle item and the last item of the array
@@ -98,6 +98,8 @@
 // console.log(webTechs);
 // console.log(contries);
 
+
+
 // Question : First remove all the punctuations and change the string to array and count the number of words in the array
 
 // let text = 'I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.'
@@ -108,3 +110,148 @@
 // let word = punctuation.split(" ")
 // console.log(word);
 // console.log(word.length);
+
+
+//Question : 
+// In the following shopping cart add, remove, edit items
+// const shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey']
+// add 'Meat' in the beginning of your shopping cart if it has not been already added
+// add Sugar at the end of you shopping cart if it has not been already added
+// remove 'Honey' if you are allergic to honey
+// modify Tea to 'Green Tea'
+
+
+// const shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey'];
+// shoppingCart.unshift("Meat");
+// shoppingCart.push('Sugar');
+// let allergic = prompt("Are You Allergic To Honey : ");
+// if (allergic == "yes") {
+//     console.log(shoppingCart.filter(remove => remove !== "Honey"));
+// } else {
+//     console.log(shoppingCart);
+// }
+// let index = shoppingCart.indexOf("Tea");
+// shoppingCart[index] = "Green Tea";
+// console.log(shoppingCart);
+
+
+// Question : In countries array check if 'Ethiopia' exists in the array if it exists print 'ETHIOPIA'. If it does not exist add to the countries list.
+// const countries = [
+//   'Albania',
+//   'Bolivia',
+//   'Canada',
+//   'Denmark',
+//   'Ethiopia',
+//   'Finland',
+//   'Germany',
+//   'Hungary',
+//   'Ireland',
+//   'Japan',
+//   'Kenya'
+// ]
+
+// if(countries.includes("Ethiopia")){
+//     console.log("ETHIOPIA");
+// }else{
+//     countries.push("Ethiopia");
+// }
+
+
+
+// // Question : In the webTechs array check if Sass exists in the array and if it exists print 'Sass is a CSS preprocess'. If it does not exist add Sass to the array and print the array.
+
+// const webTechs = [
+//   'HTML',
+//   'CSS',
+//   'JavaScript',
+//   'React',
+//   'Redux',
+//   'Node',
+//   'MongoDB'
+// ]
+
+// if(webTechs.includes("Sass")){
+//     console.log("Sass Is A CSS Processer");
+// }else{
+//     webTechs.push("Sass")
+//     console.log(webTechs);
+// }
+
+
+
+// Question : The following is an array of 10 students ages:
+
+// const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+// Sort the array and find the min and max age
+// Find the median age(one middle item or two middle items divided by two)
+// Find the average age(all items divided by number of items)
+// Find the range of the ages(max minus min)
+// Compare the value of (min - average) and (max - average), use abs() method 1.Slice the first ten countries from the countries array
+
+// 1
+const ages = [19 , 22 , 19 , 24 , 20 , 25 , 26 , 24 , 25 , 24];
+// ages.sort();
+// let maximum = ages[ages.length - 1];
+// let miniumum = ages[0];
+// console.log(`${maximum} and ${miniumum}`);
+
+
+// 2
+// ages.sort()
+// if(ages.length % 2 === 0){
+//     let Middle = Math.floor(ages.length / 2);
+//     let Median = (ages[Middle - 1] + ages[Middle]) / 2
+//     console.log(Median);
+// }else{
+//     let Middle = Math.floor(ages.length / 2);
+//     console.log(ages[Middle]);
+// }
+
+//3
+// let average = 0
+// let sum = 0
+// for(let i = 0; i < ages.length; i++){
+//     sum += ages[i]
+    
+// }
+// console.log(sum);
+// average = sum / ages.length
+// console.log(average);
+
+// 4
+let Max = ages[ages.length - 1];
+let Min = ages[0];
+let range = Max - Min;
+console.log(range);
+
+//5 
+
+let average = 0
+let sum = 0
+for(let i = 0; i < ages.length; i++){
+    sum += ages[i]
+}
+average = sum / ages.length
+
+let first_value = Math.abs(Max - average);
+let second_value = Math.abs(Min - average);
+console.log(first_value +" "+ second_value);
+console.log(first_value > second_value);
+
+// 6
+const countries = [
+  'Albania',
+  'Bolivia',
+  'Canada',
+  'Denmark',
+  'Ethiopia',
+  'Finland',
+  'Germany',
+  'Hungary',
+  'Ireland',
+  'Japan',
+  'Kenya'
+]
+
+console.log(countries.slice(0 , 10));
+
